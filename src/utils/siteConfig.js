e5ab206516ff2f2ade856624a0c481c84ca7d568
Mariangelica Useche <mus@Mariangelicas-MacBook-Pro.local>
2020-05-07 15:08:10 -0500

    [ADD] Gatsby Blog

---
+++ src/utils/siteConfig.js
@@ -0,0 +1,16 @@
+module.exports = {
+    siteUrl: `https://musarte-blog.herokuapp.com`, // Site domain. Do not include a trailing slash!
+
+    postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
+
+    siteTitleMeta: `Musarte`, // This allows an alternative site title for meta data for pages.
+    siteDescriptionMeta: `Coding life...`, // This allows an alternative site description for meta data for pages.
+
+    shareImageWidth: 1000, // Change to the width of your default share image
+    shareImageHeight: 523, // Change to the height of your default share image
+
+    shortTitle: `Musarte`, // Used for App manifest e.g. Mobile Home Screen
+    siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
+    backgroundColor: `#e9e9e9`, // Used for Offline Manifest
+    themeColor: `#15171A`, // Used for Offline Manifest
+}
