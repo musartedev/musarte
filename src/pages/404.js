9b63e9ade42edb79e1b6d34f3ef1077056669631
Mariangelica Useche <mariangelica.useche@gmail.com>
2020-05-21 22:44:42 -0500

    Update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,31 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import { Layout } from '../components/common'
+import React from "react"
+import { Link } from "gatsby"
+import { Layout } from "../components/common"
 
 const NotFoundPage = () => (
     <Layout>
         <div className="container">
             <article className="content" style={{ textAlign: `center` }}>
-                <h1 className="content-title">Error 404</h1>
+                <h1 className="content-title">4🤔4</h1>
                 <section className="content-body">
-                    Page not found, <Link to="/">return home</Link> to start over
+                    <p>
+                        Creo que estás un poco perdido. ¿Quieres{` `}
+                        <Link to="/">volver al inicio</Link>?
+                    </p>
+                    <p>
+                        También puedes seguirme en{` `}
+                        <a target="_blank" href="http://twitter.com/musarte">
+                            Twitter
+                        </a>{` `}
+                        e{` `}
+                        <a
+                            target="_blank"
+                            href="http://instagram.com/musarte.dev"
+                        >
+                            Instagram
+                        </a>
+                        .
+                    </p>
                 </section>
             </article>
         </div>
