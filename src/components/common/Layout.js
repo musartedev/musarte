2f650956a004e7b0007cc0ac2b11b7baa0991fe1
Mariangelica Useche <mariangelica.useche@gmail.com>
2020-06-15 21:54:23 -0500

    add Dark Mode 🌜

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -1,44 +1,39 @@
-import React, { useState } from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
-import { Link, StaticQuery, graphql } from 'gatsby'
-import Img from 'gatsby-image'
+import React from "react"
+import PropTypes from "prop-types"
+import Helmet from "react-helmet"
+import { Link, StaticQuery, graphql } from "gatsby"
+import { ThemeToggler } from "gatsby-plugin-dark-mode"
 
-import { Navigation } from '.'
-import config from '../../utils/siteConfig'
+import { Navigation } from "."
+import config from "../../utils/siteConfig"
+import MusarteLogo from './MusarteLogo'
 
 // Styles
-import '../../styles/app.css'
+import "../../styles/app.css"
+import "../../styles/icons.css"
 
 /**
-* Main layout component
-*
-* The Layout component wraps around each page and template.
-* It also provides the header, footer as well as the main
-* styles, and meta data for each page.
-*
-*/
-const DefaultLayout = ({ data, children, bodyClass }) => {
-    const [darkMode, setDarkMode] = useState(false);
-
-    const toggleDarkMode = () => setDarkMode(!darkMode);
-
-    const site = data.allGhostSettings.edges[0].node;
+ * Main layout component
+ *
+ * The Layout component wraps around each page and template.
+ * It also provides the header, footer as well as the main
+ * styles, and meta data for each page.
+ *
+ */
+const DefaultLayout = ({ data, children }) => {
+    const site = data.allGhostSettings.edges[0].node
     const twitterUrl = site.twitter
         ? `https://twitter.com/${site.twitter.replace(/^@/, ``)}`
-        : null;
-    const instagramUrl = `https://www.instagram.com/musarte.dev`;
+        : null
+    const instagramUrl = `https://www.instagram.com/musarte.dev`
+    const githubUrl = `https://github.com/musenberg404`
 
     return (
         <>
             <Helmet>
                 <html lang={site.lang} />
                 <style type="text/css">{`${site.codeinjection_styles}`}</style>
-                <body
-                    className={`${bodyClass} ${
-                        darkMode ? `is-dark-mode` : `is-light-mode`
-                    }`}
-                />
+                <body />
             </Helmet>
 
             <div className="viewport">
@@ -49,59 +44,87 @@ const DefaultLayout = ({ data, children, bodyClass }) => {
                             <div className="site-mast">
                                 <div className="site-mast-left">
                                     <Link to="/">
-                                        {site.logo ? (
-                                            <img
-                                                className="site-logo"
-                                                src={site.logo}
-                                                alt={site.title}
-                                            />
-                                        ) : (
-                                            <Img
-                                                fixed={
-                                                    data.file.childImageSharp
-                                                        .fixed
-                                                }
-                                                alt={site.title}
-                                            />
-                                        )}
-                                        <span
-                                            className="site-desc"
-                                            onClick={toggleDarkMode}
-                                        >
-                                            Mariangélica Useche
-                                        </span>
+                                        <MusarteLogo className={`site-logo`} width={100} />
                                     </Link>
                                 </div>
                                 <div className="site-mast-right">
-                                    { site.twitter && <a href={ twitterUrl } className="site-nav-item" target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/twitter.svg" alt="Twitter" /></a>}
-                                    <a href={ instagramUrl } className="site-nav-item" target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/instagram.svg" alt="Instagram" /></a>
-                                    <a className="site-nav-item" href={ `https://feedly.com/i/subscription/feed/${config.siteUrl}/rss/` } target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/rss.svg" alt="RSS Feed" /></a>
+                                    <div className="site-mast-social">
+                                        {site.twitter && (
+                                            <a
+                                                href={twitterUrl}
+                                                className="site-nav-item"
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                            >
+                                                <span className="site-nav-icon icon-twitter" />
+                                            </a>
+                                        )}
+                                        <a
+                                            href={instagramUrl}
+                                            className="site-nav-item"
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                        >
+                                            <span className="site-nav-icon icon-instagram" />
+                                        </a>
+                                        <a
+                                            className="site-nav-item"
+                                            href={`https://feedly.com/i/subscription/feed/${config.siteUrl}/rss/`}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                        >
+                                            <span className="site-nav-icon icon-rss" />
+                                        </a>
+                                        <a
+                                            className="site-nav-item"
+                                            href={githubUrl}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                        >
+                                            <span className="site-nav-icon icon-github" />
+                                        </a>
+                                    </div>
+                                    <ThemeToggler>
+                                        {({ theme, toggleTheme }) => (
+                                            <div className="dark-mode">
+                                                <input
+                                                    type="checkbox"
+                                                    className="checkbox"
+                                                    id="checkbox"
+                                                    onChange={e => toggleTheme(
+                                                        e.target.checked
+                                                            ? `dark`
+                                                            : `light`
+                                                    )
+                                                    }
+                                                    checked={theme === `dark`}
+                                                >
+                                                </input>
+                                                <label
+                                                    className="switch"
+                                                    htmlFor="checkbox"
+                                                >
+                                                    {theme !== `dark` && <span className="dark-mode-icon">🌜</span>}
+                                                    {theme === `dark` && <span className="light-mode-icon">☀️</span>}
+                                                </label>
+                                            </div>
+                                        )}
+                                    </ThemeToggler>
                                 </div>
-                                <div className="dark-mode">
-                                    <input
-                                        type="checkbox"
-                                        className="checkbox"
-                                        id="checkbox"
-                                        onChange={toggleDarkMode}
-                                    />
-                                    <label
-                                        className="switch"
-                                        htmlFor="checkbox"
-                                    ></label>
-                                </div>
+
                             </div>
                             <nav className="site-nav">
                                 <div className="site-nav-left">
-                                    <Navigation data={site.navigation} navClass="site-nav-item" />
+                                    <Navigation
+                                        data={site.navigation}
+                                        navClass="site-nav-item"
+                                    />
                                 </div>
                             </nav>
                         </div>
                     </header>
 
-                    <main className="site-main">
-                        {children}
-                    </main>
-
+                    <main className="site-main">{children}</main>
                 </div>
 
                 <div className="viewport-bottom">
@@ -112,12 +135,13 @@ const DefaultLayout = ({ data, children, bodyClass }) => {
                                 Hecho con 💜 por
                                 <a
                                     className="site-foot-nav-item"
-                                    href="https://github.com/musenberg404"
+                                    href={githubUrl}
                                     target="_blank"
                                     rel="noopener noreferrer"
                                 >
                                     Mariangélica Useche
-                                </a> © 2020
+                                </a>{` `}
+                                © 2020
                             </div>
                             <div className="site-foot-nav-right">
                                 <Navigation
@@ -130,8 +154,8 @@ const DefaultLayout = ({ data, children, bodyClass }) => {
                 </div>
             </div>
         </>
-    );
-};
+    )
+}
 
 DefaultLayout.propTypes = {
     children: PropTypes.node.isRequired,
@@ -154,7 +178,7 @@ const DefaultLayoutSettingsQuery = props => (
                         }
                     }
                 }
-                file(relativePath: {eq: "ghost-icon.png"}) {
+                file(relativePath: { eq: "ghost-icon.png" }) {
                     childImageSharp {
                         fixed(width: 30, height: 30) {
                             ...GatsbyImageSharpFixed
