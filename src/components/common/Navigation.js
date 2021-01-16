77f706f3956e7292bcf302dc9e58db23677b8446
Mariangelica Useche <mariangelica.useche@gmail.com>
2021-01-16 13:18:49 -0500

    Add active link indicator on the menu
    cc: This was motivated by my reading of Don't Make Me Think <3

--- src/components/common/Navigation.js
+++ src/components/common/Navigation.js
@@ -1,24 +1,43 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link } from 'gatsby'
+import React from "react"
+import PropTypes from "prop-types"
+import { Link } from "gatsby"
 
 /**
-* Navigation component
-*
-* The Navigation component takes an array of your Ghost
-* navigation property that is fetched from the settings.
-* It differentiates between absolute (external) and relative link (internal).
-* You can pass it a custom class for your own styles, but it will always fallback
-* to a `site-nav-item` class.
-*
-*/
+ * Navigation component
+ *
+ * The Navigation component takes an array of your Ghost
+ * navigation property that is fetched from the settings.
+ * It differentiates between absolute (external) and relative link (internal).
+ * You can pass it a custom class for your own styles, but it will always fallback
+ * to a `site-nav-item` class.
+ *
+ */
 const Navigation = ({ data, navClass }) => (
     <>
         {data.map((navItem, i) => {
             if (navItem.url.match(/^\s?http(s?)/gi)) {
-                return <a className={navClass} href={navItem.url} key={i} target="_blank" rel="noopener noreferrer">{navItem.label}</a>
+                return (
+                    <a
+                        className={navClass}
+                        href={navItem.url}
+                        key={i}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        {navItem.label}
+                    </a>
+                )
             } else {
-                return <Link className={navClass} to={navItem.url} key={i}>{navItem.label}</Link>
+                return (
+                    <Link
+                        className={navClass}
+                        activeClassName="active"
+                        to={navItem.url}
+                        key={i}
+                    >
+                        {navItem.label}
+                    </Link>
+                )
             }
         })}
     </>
@@ -33,7 +52,7 @@ Navigation.propTypes = {
         PropTypes.shape({
             label: PropTypes.string.isRequired,
             url: PropTypes.string.isRequired,
-        }).isRequired,
+        }).isRequired
     ).isRequired,
     navClass: PropTypes.string,
 }
