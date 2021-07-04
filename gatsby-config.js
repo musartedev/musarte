3bb39f36b803f2eaac1bdc379b29039b605830dc
Mariangelica Useche <mariangelica.useche@gmail.com>
2021-07-03 20:43:03 -0500

    This line doesn't work on netlify :hidethepain:

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,7 +79,7 @@ module.exports = {
                 background_color: config.backgroundColor,
                 theme_color: config.themeColor,
                 display: `minimal-ui`,
-                icon: `static/${config.siteIcon}`,
+                // icon: `static/${config.siteIcon}`,
                 legacy: true,
                 query: `
                 {
